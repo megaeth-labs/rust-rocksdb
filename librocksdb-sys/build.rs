be582015d36ef6cad1912803855a5aaf44c378c4
WangLang <wonder_lawrence@163.com>
2024-09-27 07:45:54 +0000

    add megaeth patch.

--- librocksdb-sys/build.rs
+++ librocksdb-sys/build.rs
@@ -53,6 +53,7 @@ fn build_rocksdb() {
     config.include("rocksdb/");
     config.include("rocksdb/third-party/gtest-1.8.1/fused-src/");
 
+    config.define("ROCKSDB_SCHED_GETCPU_PRESENT", None);
     if cfg!(feature = "snappy") {
         config.define("SNAPPY", Some("1"));
         config.include("snappy/");
